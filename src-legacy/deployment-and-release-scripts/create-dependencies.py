53605281cc50d67a6f72e2d7f577052c7e2b222c
heetbeet <sfstreicher@gmail.com>
2021-08-24 14:07:09 +0200

    Update create-dependencies.py

--- src-legacy/deployment-and-release-scripts/create-dependencies.py
+++ src-legacy/deployment-and-release-scripts/create-dependencies.py
@@ -33,7 +33,7 @@ def create_all_dependencies():
 
     # legacy support for spelling mistake "Dependancies"
     spellfix = "Dependencies" if "Dependencies" in config else "Dependancies"
-    for key, value in config[spellfix].items():
+    for key, value in get(config, spellfix, {}).items():
 
         # install python (if used)
         # add pip stuff, add logging information
