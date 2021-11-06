a896f4890cabead47855c71877ea0c00ee636bf6
heetbeet <sfstreicher@gmail.com>
2021-11-06 16:12:22 +0200

    Update main.py

--- app_builder/main.py
+++ app_builder/main.py
@@ -6,7 +6,7 @@ from locate import allow_relative_location_imports
 allow_relative_location_imports("..")
 from app_builder import exec_py
 
-def compatable_app_builder_caller():
+def caller_version_tuple():
     py_caller = Path(sys.executable).resolve()
     app_dir = py_caller.parent.parent.parent
     if app_dir.name != "app-builder":
@@ -35,20 +35,18 @@ def compatable_app_builder_caller():
     if not len(version) == 3:
         return None
 
-    if version >= (0,1,1):
-        return True
-    else:
-        return False
+    return version
 
 
 if __name__ == "__main__":
 
-    compat = compatable_app_builder_caller()
+    vertup = caller_version_tuple()
 
-    # For now shadow the legacy application
-    if compat in (None, True):
+    if vertup is None or vertup >= (0.1.0):
         exec_py.exec_py(this_dir().joinpath("src-legacy", "tools", "application.py"), globals())
+
     else:
-        print("Error: this version requires app-builder installer v0.1.1 or higher")
-        print("Download and install here: https://github.com/AutoActuary/app-builder/releases")
+        print()
+        print("Error: this version requires an installation of App-Builder-v0.1.0.exe or higher")
+        print("Please Download and install here: https://github.com/AutoActuary/app-builder/releases")
         sys.exit(-1)
