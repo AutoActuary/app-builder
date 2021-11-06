b706fe6d611468fa37521d36e4dd33957b5d4416
heetbeet <sfstreicher@gmail.com>
2021-11-06 16:15:43 +0200

    Error with wrong entry version (#16)
    * Add error message to app-builder
    
    * Bugfix "v"
    
    * add pathlib
    
    * fixed version checker
    
    * Update main.py
    
    * Update main.py
    
    * Update application.yaml

--- app_builder/main.py
+++ app_builder/main.py
@@ -1,10 +1,52 @@
 from locate import this_dir
 import subprocess
 import sys
+from pathlib import Path
 from locate import allow_relative_location_imports
 allow_relative_location_imports("..")
 from app_builder import exec_py
 
+def caller_version_tuple():
+    py_caller = Path(sys.executable).resolve()
+    app_dir = py_caller.parent.parent.parent
+    if app_dir.name != "app-builder":
+        return None
+
+    version_link = list(app_dir.glob("GitHub commit *.lnk"))
+    if len(version_link) != 1:
+        return None
+    version_link = version_link[0]
+
+    version = version_link.name.split("GitHub commit v", 1)[-1].split(".lnk", 1)[0]
+    version = version.split("-")[0]
+    version = tuple(version.split("."))
+
+    if not len(version) == 3:
+        return None
+
+    def int_able(x):
+        try:
+            int(x)
+            return True
+        except:
+            return False
+
+    version = tuple(int(i) for i in version if int_able(i))
+    if not len(version) == 3:
+        return None
+
+    return version
+
+
 if __name__ == "__main__":
-    # For now shadow the legacy application
-    exec_py.exec_py(this_dir().joinpath("src-legacy", "tools", "application.py"), globals())
+
+    vertup = caller_version_tuple()
+
+    if vertup is None or vertup >= (0, 1, 0):
+        exec_py.exec_py(this_dir().joinpath("src-legacy", "tools", "application.py"), globals())
+
+    else:
+        print()
+        print("Error: this version requires an installation of App-Builder-v0.1.0.exe or higher")
+        print("Please Download and install here: https://github.com/AutoActuary/app-builder/releases")
+        sys.exit(-1)
