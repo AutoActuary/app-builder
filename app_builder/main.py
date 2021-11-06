17055271ee50d4d51569c2923a5a7c3f02c295bd
heetbeet <sfstreicher@gmail.com>
2021-11-06 13:28:22 +0200

    Add application entry as python script

--- app_builder/main.py
+++ app_builder/main.py
@@ -1,8 +1,8 @@
 from locate import this_dir
 import subprocess
 import sys
+from exec_py import exec_py
 
 if __name__ == "__main__":
-    # For now shadow the previous legacy application
-    sys.exit(
-        subprocess.call([str(this_dir().joinpath("src-legacy", "tools", "application-full.bat"))]+sys.argv[1:]))
+    # For now shadow the legacy application
+    exec_py(this_dir().joinpath("src-legacy", "tools", "application.py"), globals())
