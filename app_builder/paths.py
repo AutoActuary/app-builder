ab2d30b62653096783482c4b9c23098b2b678db8
dolf <rudolfbyker@gmail.com>
2026-02-17 16:38:39 +0200

    Stop using `locate` in paths.py

--- app_builder/paths.py
+++ app_builder/paths.py
@@ -3,14 +3,12 @@ import sys
 import tempfile
 from pathlib import Path
 
-from locate import this_dir
-
 installed_dir = Path(
     os.environ["LOCALAPPDATA"] if sys.platform == "win32" else "/opt",
     "autoactuary",
     "app-builder",
 ).resolve()
-base_dir = this_dir().parent
+base_dir = Path(__file__).resolve().parent
 temp_dir = Path(tempfile.gettempdir(), f"app-builder-7dfd13678769").resolve()
 
 
