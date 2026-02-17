b11c5cc510c121d674de118f513e71ae5a9e33a0
dolf <rudolfbyker@gmail.com>
2026-02-18 00:28:01 +0200

    Allow users to override the release version on the command line.

--- app_builder/commands/release.py
+++ app_builder/commands/release.py
@@ -4,8 +4,19 @@ from ..create_release import create_release
 
 
 @click.command()
-def release() -> None:
+@click.option(
+    "--version",
+    type=str,
+    help="The version for the release. "
+    "If not provided, the version will be detected automatically from `git describe --tags` if possible, "
+    "or else fall back to `unknown`.",
+    default=None,
+)
+def release(
+    *,
+    version: str | None,
+) -> None:
     """
     Create a release.
     """
-    create_release()
+    create_release(version=version)
