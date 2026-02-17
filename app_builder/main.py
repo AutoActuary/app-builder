db7239819b6886c63210e9b9f69f51f8829f3eb3
dolf <rudolfbyker@gmail.com>
2026-02-17 16:18:43 +0200

    Inline application.py into main.py

--- app_builder/main.py
+++ app_builder/main.py
@@ -1,9 +1,9 @@
 import sys
 from pathlib import Path
 
-this_dir = Path(__file__).resolve().parent
-sys.path.insert(0, this_dir.parent.as_posix())  # parent dir
-from app_builder import exec_py
+from locate import append_sys_path
+
+from .util import help, init
 
 
 def caller_version_tuple():
@@ -43,17 +43,9 @@ def caller_version_tuple():
     return version
 
 
-if __name__ == "__main__":
-
-    vertup = caller_version_tuple()
-
-    if vertup is None or vertup >= (0, 1, 0):
-        exec_py.exec_py(
-            this_dir.joinpath("src-legacy", "tools", "application.py"),
-            globals(),
-        )
-
-    else:
+def main() -> None:
+    version = caller_version_tuple()
+    if version is not None and version < (0, 1, 0):
         print()
         print(
             "Error: this version requires an installation of App-Builder-v0.1.0.exe or higher"
@@ -62,3 +54,36 @@ if __name__ == "__main__":
             "Please Download and install here: https://github.com/AutoActuary/app-builder/releases"
         )
         sys.exit(-1)
+
+    # Reshuffle args in order to call future scripts
+    command = "-h" if len(sys.argv) < 2 else sys.argv[1].lower()
+    sys.argv = sys.argv[0:1] + sys.argv[2:]
+
+    if command in ("-h", "--help"):
+        help()
+
+    elif command in ("-i", "--init"):
+        init()
+
+    elif command in ("-d", "--get-dependencies"):
+        with append_sys_path("../deployment-and-release-scripts"):
+            from get_dependencies import get_dependencies
+        get_dependencies()
+
+    elif command in ("-l", "--local-release"):
+        with append_sys_path("../deployment-and-release-scripts"):
+            from create_releases import create_releases
+        create_releases()
+
+    elif command in ("-g", "--github-release"):
+        with append_sys_path("../deployment-and-release-scripts"):
+            from create_github_release import create_github_release
+        create_github_release()
+
+    else:
+        print("Error: wrong commandline arguments")
+        help()
+
+
+if __name__ == "__main__":
+    main()
